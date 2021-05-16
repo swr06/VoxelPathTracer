009c5febb488a47442683b36a36b8d2710a5a3cb
Samuel Rasquinha <samuelrasquinha@gmail.com>
2021-05-16 18:15:12 +0530

    Added pipeline (Moved code out of main.cpp)

---
+++ Core/Pipeline.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include <iostream>
+
+#include "Application/Application.h"
+#include "GLClasses/VertexArray.h"
+#include "GLClasses/VertexBuffer.h"
+#include "GLClasses/IndexBuffer.h"
+#include "GLClasses/Shader.h"
+#include "FpsCamera.h"
+#include "GLClasses/Fps.h"
+#include "World.h"
+#include "GLClasses/Framebuffer.h"
+#include "WorldGenerator.h"
+#include "InitialTraceFBO.h"
+#include "GLClasses/CubeTextureMap.h"
+#include "BlockDatabase.h"
+#include "Player.h"
+#include "GLClasses/FramebufferRed.h"
+#include "ColorPassFBO.h"
+#include "GLClasses/Texture.h"
+#include "Renderer2D.h"
+#include "WorldFileHandler.h"
+#include "AtmosphereRenderer.h"
+#include "AtmosphereRenderCubemap.h"
+
+namespace VoxelRT
+{
+	namespace MainPipeline
+	{
+		void StartPipeline();
+	}
+}
